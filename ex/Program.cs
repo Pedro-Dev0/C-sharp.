4cfdbcadb3b603b15dbaaeaae5635d50e7a4ff69
Pedro Sampaio <208987957+Pedro-Dev0@users.noreply.github.com>
2025-07-03 18:36:33 -0300

    c-sharp

--- ex/Program.cs
+++ ex/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+{
+    int primeironumero = 50;
+    string primeirotexto = "Olá mundo";
+    float primeirofloat = 50.22;
+}
